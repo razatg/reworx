33dec7112aa99f210a708a8b29c0de2288a253cc
muraris4u <pradip.comat@gmail.com>
2017-12-12 17:28:02 +0530

    Add fresh copy of project

--- config-ini.php
+++ config-ini.php
@@ -10,9 +10,9 @@ else if($_SERVER['HTTP_HOST']=='demo.onsisdev.info')
 	$angRoute = "http://demo.onsisdev.info/tracking";
 }
 define("ANGULAR_ROUTE",$angRoute);
-define("SENDGRID_URL","https://api.sendgrid.com/");
-define("SENDGRID_KEY","referralworx");
-define("SENDGRID_PASS","refer1!worx");
+define("SENDGRID_URL","");
+define("SENDGRID_KEY","");
+define("SENDGRID_PASS","");
 define("ANGULAR_ROUTE",$angRoute);
 define("EMAIL_ALLREADY","An account for the specified email address already exists. Try another email address.");
 define("SUCCESSFULLY_REGISTER","Thank You. We will get back to you Soon");
