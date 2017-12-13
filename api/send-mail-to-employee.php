f542d494c6fef0da2ada7db03de30ef94ecbb3b7
muraris4u <pradip.comat@gmail.com>
2017-12-13 14:33:54 +0530

    added gitignore

---
+++ api/send-mail-to-employee.php
@@ -0,0 +1,144 @@
+<?php 
+include_once('../config-ini.php');
+$body = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>TheReferralWorks</title>
+</head>
+<body style="margin:0px; padding:0px;" bgcolor="#cccccc">
+<style>
+.emailer_temp{max-width:600px; margin:15px auto;}
+@media screen and (max-width: 600px) {
+.emailer_temp{max-width:90%; margin:15px auto;}
+}
+</style>
+<table class="emailer_temp" width="600px" bgcolor="#ffffff" border="0" cellspacing="0" cellpadding="0" align="center" style="border: 1px solid #bccbcb;">
+   <tr>
+      <td align="center" bgcolor="#279eb6">
+         <table width="100%" border="0" cellspacing="15" cellpadding="0">
+            <tr>
+               <td><a href="#" target="_blank"><img border="0" alt="" src="'.ANGULAR_ROUTE.'/newui/images/logo.png"/></a></td>
+            </tr>
+         </table>
+      </td>
+   </tr>
+   <tr>
+      <td>
+         <table width="100%" border="0" cellspacing="15" cellpadding="0">
+            <tr>
+               <td valign="top">
+                  <h1 style="font-size:20px; font-family:calibri,arial,sans-serif; color:#414747; padding:0px 0; line-height:normal; margin:0; margin-bottom:5px;">
+                  </h1>
+                  <p style="font-size:16px; font-family:calibri,arial,sans-serif; color:#414747; line-height:22px; margin:0 0 20px 0">__MSG_CONTENT__</p>
+               </td>
+            </tr>
+            <tr>
+            </tr>
+         </table>
+      </td>
+   </tr>
+   <tr>
+      <td bgcolor="#e5eded" style="border-top: 1px solid #bccbcb;">
+         <table width="100%" border="0" cellspacing="13" cellpadding="0">
+            <tr>
+               <td style="font-size:14px; font-family:calibri,arial,sans-serif; color:#819191; line-height:20px; margin:0; text-align:center;">© 2017 TheReferralWorks. All rights reserved.</td>
+            </tr>
+         </table>
+      </td>
+   </tr>
+</table>
+</body>
+</html>';
+$returnArr = array('status'=>'failure');
+$userData = json_decode(file_get_contents('php://input'), true);
+if(!empty($userData))
+{
+	$db = connect();
+	$data = $userData['formdata'];
+	if($_SERVER['HTTP_HOST']=='demo.onsisdev.info')
+	{
+		$to = array('to'=>array("pradip.comat@gmail.com","girdhar.rajat@gmail.com"));
+	}
+	else
+	{
+		$to = array('to'=>array($data['employeeDetail']['profile'][0]['email']));
+	}
+	$addedOn = $data['employeeDetail']['time'];
+	$cId = $data['employeeDetail']['cId'];
+	$ccArry = array();
+	if(!empty($cId))
+	{
+		$dataListArr = $db->recruiter->findOne(array('cId'=>(int)$cId));
+		if(!empty($dataListAr))
+		{
+			$ccArry = array($dataListAr['email']);
+		}
+	}
+	$messageHTML =  $data['message_to_employee'];
+	$messageHTML = str_replace('__MSG_CONTENT__',nl2br($messageHTML),$body);
+	$from = !empty($_SESSION['member']['email'])?$_SESSION['member']['email']:"";
+	$fromName = !empty($_SESSION['member']['first_name'])?$_SESSION['member']['first_name']:"";
+	$toname   =  $data['employeeDetail']['profile'][0]['name'];
+	$subject  =  $data['subject_to_employee'];
+	$messageText = '';
+	sendgridmail($from, $fromName, $to, $toname, $subject, $messageText, $messageHTML, array(),$ccArry);
+	$db = connect();
+	if(!empty($_SESSION['member']['UID']))
+	{
+		$UID = $_SESSION['member']['UID'];
+		$UIDFromRemove = $data['employeeDetail']['profile'][0]['UID']; 
+		$dataListArr = $db->employeeReferData->findOne(array('addedOn'=>(int)$addedOn));
+		$uIdList = $dataListArr['referalUIDList'];
+		if(!empty($uIdList))
+		{   $newUpdatedArr =  array(); 
+			foreach($uIdList as $item)
+			{
+				if($item['UID']== $UIDFromRemove)
+				{
+					$item['fit'] = true; 
+				}
+			   $newUpdatedArr[] = $item;
+			}
+		}
+		$updateArr = array('$set'=>array("referalUIDList"=>$newUpdatedArr));
+		$db->employeeReferData->update(array('addedOn'=>(int)$addedOn),$updateArr);
+	}
+	
+	echo 'success';exit;
+}
+
+function sendgridmail($from, $fromName, $json_string, $toname, $subject, $messageText, $messageHTML, $headers, $ccArry) 
+{
+	$url = SENDGRID_URL;
+	$user = SENDGRID_KEY;
+	$pass = SENDGRID_PASS;
+    $params = array(
+							'api_user'  => $user,
+							'api_key'   => $pass,
+							'x-smtpapi' => json_encode($json_string),
+							'to'        => 'noreply@referralworx.com',
+							'cc'        => json_encode($json_string),
+							'subject'   => $subject,
+							'html'      => $messageHTML,
+							'from'      => $fromName.' via Referralworx <'.$from.'>',
+			               );
+			$request =  $url.'api/mail.send.json';
+			// Generate curl request
+			$session = curl_init($request);
+			// Tell curl to use HTTP POST
+			curl_setopt ($session, CURLOPT_POST, true);
+			// Tell curl that this is the body of the POST
+			curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+			// Tell curl not to return headers, but do return the response
+			curl_setopt($session, CURLOPT_HEADER, false);
+			// Tell PHP not to use SSLv3 (instead opting for TLS)
+			curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+			curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+			// obtain response
+			$response = curl_exec($session);
+			curl_close($session);
+			// print everything out
+			//print_r($response);
+}
+?>
---
+++ api/sendmail.php
@@ -0,0 +1,148 @@
+<?php
+ini_set('display_errors',0);
+include_once('../config-ini.php');
+$body = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>TheReferralWorks</title>
+</head>
+<body style="margin:0px; padding:0px;" bgcolor="#cccccc">
+<style>
+.emailer_temp{max-width:600px; margin:15px auto;}
+@media screen and (max-width: 600px) {
+.emailer_temp{max-width:90%; margin:15px auto;}
+}
+</style>
+<table class="emailer_temp" width="600px" bgcolor="#ffffff" border="0" cellspacing="0" cellpadding="0" align="center" style="border: 1px solid #bccbcb;">
+   <tr>
+      <td align="center" bgcolor="#279eb6">
+         <table width="100%" border="0" cellspacing="15" cellpadding="0">
+            <tr>
+               <td><a href="#" target="_blank"><img border="0" alt="" src="'.ANGULAR_ROUTE.'/newui/images/logo.png"/></a></td>
+            </tr>
+         </table>
+      </td>
+   </tr>
+   <tr>
+      <td>
+         <table width="100%" border="0" cellspacing="15" cellpadding="0">
+            <tr>
+               <td valign="top">
+                  <h1 style="font-size:20px; font-family:calibri,arial,sans-serif; color:#414747; padding:0px 0; line-height:normal; margin:0; margin-bottom:5px;">
+                  </h1>
+                  <p style="font-size:16px; font-family:calibri,arial,sans-serif; color:#414747; line-height:22px; margin:0 0 20px 0">__MSG_CONTENT__</p>
+               </td>
+            </tr>
+            <tr>
+            	<td><a href="__LINK_URL__" style="background: #4ec8b0;color: #fff;font-size: 20px;text-align: center;padding: 0px 20px;border-radius: 7px;overflow: hidden;text-decoration: navajowhite;display: table;margin: 15px auto;width: auto;height: 40px;font-family: arial;line-height: 20px;">Refer</a></td>
+            </tr>
+         </table>
+      </td>
+   </tr>
+   <tr>
+      <td bgcolor="#e5eded" style="border-top: 1px solid #bccbcb;">
+         <table width="100%" border="0" cellspacing="13" cellpadding="0">
+            <tr>
+               <td style="font-size:14px; font-family:calibri,arial,sans-serif; color:#819191; line-height:20px; margin:0; text-align:center;">© 2017 TheReferralWorks. All rights reserved.</td>
+            </tr>
+         </table>
+      </td>
+   </tr>
+</table>
+</body>
+</html>'; 
+$returnArr = array('status'=>'failure');
+$userData = json_decode(file_get_contents('php://input'), true);
+if(!empty($userData))
+{
+	$mailData = $userData['formdata'];
+	$db = connect();
+	$referArr = array();
+	$referArrParent = array();
+	$currentTime = time();
+	foreach($userData['connection']['data'] as $data)
+	{
+		if(!empty($data['connectedUsers']))
+		{
+			foreach($data['connectedUsers'] as $key=>$item)
+			{
+				if($item['IsChecked']==true)
+				{
+					$referArrParent[] =  array('UID'=>(int)$data['UID'],'notFit'=>false,'donotknow'=>false,'fit'=>false);
+					$referArr[$item['UID']] =  $item['UID'];
+					$to = array('to'=>array($item['email']));
+					$messageHTML = "Hi ".$item['name'].",<br>".$data['name']." seems to be a good fit for the open position that we have, ".$mailData['job_title'].". <br>Since ".$data['name']." is connected with you on your social network, requesting you to write to him and have him get in touch with me or the HR team.<br>Please Click here or the Button Below to write to him:<br><a>Refer ".$data['name']."</a>";
+					$messageHTML = str_replace('__MSG_CONTENT__',$messageHTML,$body);
+					$linkurl = ANGULAR_ROUTE.'/api/referal-auth.php?email='.base64_encode($item['email']).'&UID='.base64_encode($item['UID']).'&key='.base64_encode($data['UID']).'&uniqueID='.base64_encode($currentTime);
+					$messageHTML = str_replace('__LINK_URL__',$linkurl,$messageHTML);
+					$from = '';
+					$fromName = '';
+					$toname = '';
+					$subject = 'Refer your Connection '.$data['name'];
+					$messageText = '';
+					sendgridmail( $from, $fromName, $to, $toname, $subject, $messageText, $messageHTML, array(),array("UID"=>$data['UID']));
+				}
+			}
+		}
+	}
+	if(!empty($referArr))
+	{
+		foreach($referArr as $key=>$val)
+		{
+			$dataRefer['UID'] = $key;
+			$dataRefer['cId'] = $_SESSION['member']['cId'];
+			$dataRefer['referalUIDList'] = $referArrParent;
+			$dataRefer['recruiterList'] =  $mailData;
+			$dataRefer['addedOn'] = $currentTime;
+			$db->employeeReferData->insert($dataRefer);
+		}
+		if(!empty($_SESSION['member']['cId']))
+		{
+			$cId = $_SESSION['member']['cId'];
+			$db->recruitershortlist->remove(array("cId"=>$cId));
+		}
+	}
+	echo 'success';exit;
+}
+
+function sendgridmail( $from, $fromName, $json_string, $toname, $subject, $messageText, $messageHTML, $headers, $unique_args) 
+{
+	$email = 'invitation@referralworx.com';
+	if(!empty($_SESSION['member']['cId']))
+	{
+		$email = $_SESSION['member']['email'];
+		$company_name = $_SESSION['member']['company_name'];
+	}
+	$url = SENDGRID_URL;
+	$user = SENDGRID_KEY;
+	$pass = SENDGRID_PASS;
+    $params = array(
+					'api_user'  => $user,
+					'api_key'   => $pass,
+					'x-smtpapi' => json_encode($json_string),
+					'to'        => 'noreply@referralworx.com',
+					'subject'   => $subject,
+					'html'      => $messageHTML,
+					'from'      => $company_name.' via Referralworx <'.$email.'>',
+					'unique_args' =>json_encode($unique_args)
+				   );
+			$request =  $url.'api/mail.send.json';
+			// Generate curl request
+			$session = curl_init($request);
+			// Tell curl to use HTTP POST
+			curl_setopt ($session, CURLOPT_POST, true);
+			// Tell curl that this is the body of the POST
+			curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+			// Tell curl not to return headers, but do return the response
+			curl_setopt($session, CURLOPT_HEADER, false);
+			// Tell PHP not to use SSLv3 (instead opting for TLS)
+			curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+			curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+			// obtain response
+			$response = curl_exec($session);
+			curl_close($session);
+			// print everything out
+			//print_r($response);
+}
+?>
