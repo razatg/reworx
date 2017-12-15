36c0896dceb7646c8ec474c58ea42d8bba1d4885
muraris4u <pradip.comat@gmail.com>
2017-12-15 20:18:36 +0530

    update changes

--- page/uploadcsv.php
+++ page/uploadcsv.php
@@ -15,8 +15,8 @@ include_once('../config-ini.php');
 		<img src="newui/images/linkdenlogo.jpg" alt="">
 		<h1>Upload your LinkedIn CSV </h1>
 		<?php if(!empty($_SESSION['member']['email'])){?>
-			<div class="fileUpload application-btn">
-			<label class="upload">
+			<div class="fileUpload">
+			<label class="upload application-btn">
 				<input ng-if="!showLoder" type="file" onchange="angular.element(this).scope().setFile(this)">
 		    	Upload
 			</label>
