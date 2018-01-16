6dfdac28e4df0cebea81e72ac0aa3dc6831a1346
muraris4u <pradip.comat@gmail.com>
2018-01-16 17:42:22 +0530

    update changes

--- sendgridApi.php
+++ sendgridApi.php
@@ -1,32 +1,46 @@
 <?php
-$postdata = file_get_contents("php://input");
-$postdatanew = json_decode($postdata,true);
-if(!empty($postdatanew))
-{
-	$db = connect();
-	$insertArr = array();
-	foreach($postdatanew as $data)
-	{
-		if(!empty($data['category']))
-		{
-			$cat = explode('_',$data['category']);
-			$activityList = $db->emailActivity->findOne(array("UID"=>(int)$cat[0],"employeeId"=>(int)$cat[2],"identifier"=>(int)$cat[1]));
-			if(!empty($activityList))
+$myFile = "tmp/log.txt";
+$fh = fopen($myFile, 'a+') or die("can't open file");
+include_once('config-ini.php');
+if ($fh){
+    $headers = apache_request_headers();
+    $postdata = file_get_contents("php://input");
+    $postdatanew = json_decode($postdata,true);
+    if(!empty($postdatanew))
+    {	 $db = connect();
+		$insertArr = array();
+			foreach($postdatanew as $data)
 			{
-				$db->emailActivity->update(array("UID"=>(int)$cat[0],"employeeId"=>(int)$cat[2],"identifier"=>(int)$cat[1]),array('$set'=>array('event'=>$data['event'],'timestamp'=>(int)$data['timestamp'])));
+				if(!empty($data['category']))
+				{
+					$cat = explode('_',$data['category']);
+					$activityList = $db->emailActivity->findOne(array("UID"=>(int)$cat[0],"employeeId"=>(int)$cat[2],"identifier"=>(int)$cat[1]));
+					if(!empty($activityList))
+					{
+						$db->emailActivity->update(array("UID"=>(int)$cat[0],"employeeId"=>(int)$cat[2],"identifier"=>(int)$cat[1]),array('$set'=>array('event'=>$data['event'],'timestamp'=>(int)$data['timestamp'])));
+					}
+					else
+					{
+						
+						$insertArr['email'] = $data['email'];
+						$insertArr['timestamp'] = (int)$data['timestamp'];
+						$insertArr['UID'] = (int)$cat[0];
+						$insertArr['employeeId'] = (int)$cat[2];
+						$insertArr['identifier'] = (int)$cat[1];
+						$insertArr['event'] = $data['event'];
+						$db->emailActivity->insert($insertArr);
+					}
+				}
 			}
-			else
-			{
-				
-				$insertArr['email'] = $data['email'];
-				$insertArr['timestamp'] = (int)$data['timestamp'];
-				$insertArr['UID'] = (int)$cat[0];
-				$insertArr['employeeId'] = (int)$cat[2];
-				$insertArr['identifier'] = (int)$cat[1];
-				$insertArr['event'] = $data['event'];
-				$db->emailActivity->insert($insertArr);
-			}
-		}
 	}
+     
+    foreach ($headers as $header => $value) {
+    //fwrite($fh, print_r("$header: $value \n", true));
 }
+
+fwrite($fh, print_r("$postdata \n", true));
+fclose($fh);
+}
+
+echo "ok";
 ?>
