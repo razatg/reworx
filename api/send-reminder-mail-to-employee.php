9f0dbda8fce164b1fef2c62e8d343bbd9244032b
muraris4u <pradip.comat@gmail.com>
2018-01-16 19:35:14 +0530

    update changes

--- api/send-reminder-mail-to-employee.php
+++ api/send-reminder-mail-to-employee.php
@@ -1,6 +1,6 @@
 <?php 
 include_once('../config-ini.php');
-ini_set('display_errors',1);
+ini_set('display_errors',0);
 $body = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
